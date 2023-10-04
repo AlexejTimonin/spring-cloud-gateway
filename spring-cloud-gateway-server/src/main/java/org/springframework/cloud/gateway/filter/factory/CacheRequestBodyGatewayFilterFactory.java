f3f0a15b3f92acc63ea1f67a282523090d421b05
Alexej Timonin <alexejtimonin@gmail.com>
2023-10-04 15:06:29 +0200

    Allow for message readers customization
    Add an option to pass in user configured message readers.

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/CacheRequestBodyGatewayFilterFactory.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/CacheRequestBodyGatewayFilterFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2020 the original author or authors.
+ * Copyright 2013-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,6 +49,11 @@ public class CacheRequestBodyGatewayFilterFactory
 	public CacheRequestBodyGatewayFilterFactory() {
 		super(CacheRequestBodyGatewayFilterFactory.Config.class);
 		this.messageReaders = HandlerStrategies.withDefaults().messageReaders();
+	}
+
+	public CacheRequestBodyGatewayFilterFactory(List<HttpMessageReader<?>> messageReaders) {
+		super(CacheRequestBodyGatewayFilterFactory.Config.class);
+		this.messageReaders = messageReaders;
 	}
 
 	@Override
